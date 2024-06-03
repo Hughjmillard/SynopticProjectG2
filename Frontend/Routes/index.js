39e2af8c75fa919d3b50eb2f47ed70ffd7aaab18
Hughjmillard <hughjmillard@gmail.com>
2024-06-03 21:59:37 +0100

    trying to remember web dev

--- Frontend/Routes/index.js
+++ Frontend/Routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (request, response) => {
     response.render('index',{title: 'Health Website',
         homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
-        learningNav: 'learning',
+        learningNav: 'learning', heading: 'Heath Diagnosis Tool'
     });
 });
 
