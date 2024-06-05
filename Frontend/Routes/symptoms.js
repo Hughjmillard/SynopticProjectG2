580f87c1e8c18f87b05f2c1dcee96343cbba385d
Hughjmillard <hughjmillard@gmail.com>
2024-06-05 11:39:25 +0100

    fixed dynamic loading

--- Frontend/Routes/symptoms.js
+++ Frontend/Routes/symptoms.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (request, response) => {
     response.render('symptoms',{title: 'Health Website',
     homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
-    learningNav: 'learning',
+    learningNav: 'learning', header: 'Header test'
     });
 });
 
