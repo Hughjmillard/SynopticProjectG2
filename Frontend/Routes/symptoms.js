d30865e3e8b142cf839f598e7a3585e8938fe056
Hughjmillard <hughjmillard@gmail.com>
2024-06-05 13:48:45 +0100

    Added a submit button to symptoms page+dynamic content

--- Frontend/Routes/symptoms.js
+++ Frontend/Routes/symptoms.js
@@ -4,7 +4,9 @@ const router = express.Router();
 router.get('/', (request, response) => {
     response.render('symptoms',{title: 'Health Website',
     homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
-    learningNav: 'learning', header: 'Header test'
+    learningNav: 'learning', header: 'Diagnosis Tool', Symptom1: 'Headache', Symptom2: 'Fever',
+    Symptom3: 'Open Wound', Symptom4: 'Nausea', Symptom5: 'Loss of Appetite',
+    Symptom6: 'Night Sweats', Symptom7: 'Vomiting', Symptom8: 'Blurred Vision', button:'Submit'
     });
 });
 
