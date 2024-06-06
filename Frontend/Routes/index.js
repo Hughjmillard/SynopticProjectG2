a3fadf1ac210d530832fcf23f4ccc04daa08f612
Hughjmillard <hughjmillard@gmail.com>
2024-06-06 14:34:31 +0100

    Revert "Merge branch 'main' of https://github.com/Hughjmillard/SynopticProjectG2"
    This reverts commit 4d5cea4d6c8dc6e4be8c60d06368cf36d8bcaecb, reversing
    changes made to 42e07324d4a610f48f9f765d4f3bbdb9ea191cf8.

--- Frontend/Routes/index.js
+++ Frontend/Routes/index.js
@@ -5,9 +5,8 @@ router.get('/', (request, response) => {
     response.render('index',{title: 'Health Website',
         homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
         learningNav: 'learning', heading: 'Heath Diagnosis Tool',
-        diagnosisHeader: 'Self Diagnosis Tool',
-        // diag
-        contactHeader: 'Find a doctor'
+        whatHeader: 'Self Diagnosis Tool',
+        howHeader: 'Find a doctor'
     });
 });
 
