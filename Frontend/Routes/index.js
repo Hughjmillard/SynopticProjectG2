6b0d50ad572a70665bb2b42c677d2b41d5c68296
Hughjmillard <hughjmillard@gmail.com>
2024-06-06 14:28:36 +0100

    added onclick

--- Frontend/Routes/index.js
+++ Frontend/Routes/index.js
@@ -5,8 +5,9 @@ router.get('/', (request, response) => {
     response.render('index',{title: 'Health Website',
         homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
         learningNav: 'learning', heading: 'Heath Diagnosis Tool',
-        whatHeader: 'Self Diagnosis Tool',
-        howHeader: 'Find a doctor'
+        diagnosisHeader: 'Self Diagnosis Tool',
+        // diag
+        contactHeader: 'Find a doctor'
     });
 });
 
