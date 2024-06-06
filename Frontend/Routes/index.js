eeed2f00987d8b04b8bdcbdce662ad4ed012e00a
Hughjmillard <hughjmillard@gmail.com>
2024-06-06 15:34:26 +0100

    added descriptions to index

--- Frontend/Routes/index.js
+++ Frontend/Routes/index.js
@@ -5,8 +5,10 @@ router.get('/', (request, response) => {
     response.render('index',{title: 'Health Website',
         homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
         learningNav: 'learning', heading: 'Heath Diagnosis Tool',
-        whatHeader: 'Self Diagnosis Tool',
-        howHeader: 'Find a doctor'
+        diagnosisHeader: 'Self Diagnosis Tool',
+        diagnosisPara: 'Use this tool to input symptoms and get a diagnosis with a remedy',
+        contactHeader: 'Find a doctor',
+        contactPara: 'Use this page to get contact information for first aiders and local doctors'
     });
 });
 
