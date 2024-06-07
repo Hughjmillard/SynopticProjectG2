fe4b647af195f41400d9505ab8dbb0d66267f9ef
Logan Scully <accounts@loganscully.com>
2024-06-07 14:22:23 +0100

    changes

--- Frontend/server.js
+++ Frontend/server.js
@@ -25,3 +25,56 @@ app.listen(port,(error)=>{
         console.log(`port is running on ${port}!`);
     }
 });
+
+let mysql =require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword",
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  
+    var query1 = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    var query2 = "CREATE TABLE orders (product VARCHAR(255), quantity INT)";
+  
+    con.query(query1, function (err, result) {
+      if (err) throw err;
+      console.log("Table 'customers' created");
+      
+    con.query(query2, function (err, result) {
+        if (err) throw err;
+        console.log("Table 'orders' created");
+      });
+    });
+  });
+  
+
+  // create the database
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("CREATE DATABASE diagnosis", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
+
+
+// drop the database if it isnt working
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("drop database diagnosis", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
