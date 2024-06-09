4057f6d7a458c2a08170f69720431c6c30e7a3fa
Hughjmillard <hughjmillard@gmail.com>
2024-06-09 17:35:22 +0100

    changing contact page

--- Frontend/Routes/contact.js
+++ Frontend/Routes/contact.js
@@ -4,7 +4,18 @@ const router = express.Router();
 router.get('/', (request, response) => {
     response.render('contact',{title: 'Health Website',
     homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
-    learningNav: 'learning', heading: 'filler header'
+    learningNav: 'learning', heading: 'Contact Page',
+    contactPara: 'Shows contact information for local administrator' +
+    ', local first aider and the closest doctors office.',
+    sysAdminName:'System Administrator',
+    sysAdminAddress:'St NumberStName <br>Town <br>County <br>Postcode',
+    sysAdminPhone:'+855 73 748 8359',
+    aidName:'Local First Aider',
+    aidAddress:'St NumberStName <br>Town <br>County <br>Postcode',
+    aidPhone:'+855 73 367 9827',
+    doctorName:'Mondolkiri polyclinic',
+    doctorAddress:'St NumberStName <br>Town <br>County <br>Postcode',
+    doctorPhone:'+855 73 555 5117',
     });
 });
 
