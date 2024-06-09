a8b24df642862c49fdacad9d1c58d5ffd64f4bfa
Logan Scully <accounts@loganscully.com>
2024-06-09 17:35:05 +0100

    fsdf

--- Frontend/Routes/learning.js
+++ Frontend/Routes/learning.js
@@ -15,8 +15,14 @@ router.get('/', (request, response) => {
     shock: 'Shock Management',
 
     blsWelcome: 'Welcome to the Basic Life Support (BLS) learning module',
-    blsContent: 'This module will teach you how to perform CPR and use an AED',
-    blsCPR: 'First: Check for Responsiveness, 
+    blsContent: 'This course equips you with essential skills to respond to cardiac and respiratory emergencies. You\'ll learn high-quality chest compressions, effective ventilation, and how to use an automated external defibrillator (AED). Our hands-on training ensures you are prepared to perform life-saving interventions confidently in any emergency situation.',
+    blsCPRtitle: 'Cardiopulmonary Resuscitation (CPR)',
+    blsCPR: '1: Check Responsiveness and Call for Help. If there is no response, call emergency services immediately or ask someone nearby to call.',
+    blsCPR2: '2: Open the Airway. Tilt the head back and lift the chin to open the airway.',
+    blsCPR3: '3: Check for Breathing. Look, listen, and feel for breathing. If the person is not breathing, start CPR.',
+    blsCPR4: '4: Start Chest Compressions. Place the heel of one hand on the center of the chest and interlock the fingers. Push hard and fast at a rate of 100-120 compressions per minute.',
+    blsCPR5: '5: Give Rescue Breaths. Pinch the nose and give two breaths into the mouth. Each breath should make the chest rise.',
+    blsCPR6: '6: Continue CPR. Perform cycles of 30 chest compressions and two rescue breaths until help arrives.',
     });
 });
 
