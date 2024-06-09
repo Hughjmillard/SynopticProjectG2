d23e4600d7fc68133da01f1b74476a68d7e56abc
Logan Scully <accounts@loganscully.com>
2024-06-09 17:06:16 +0100

    woosh

--- Frontend/Routes/learning.js
+++ Frontend/Routes/learning.js
@@ -6,6 +6,17 @@ router.get('/', (request, response) => {
     homeNav: 'Home', symptomsNav: 'diagnosis tool', contactNav: 'contact doctors',
     learningNav: 'learning',
     bls: 'Basic Life Support (BLS)',
+    wound: 'Wound Care',
+    fracture: 'Fracture Care',
+    burn: 'Burn Care',
+    choking: 'Airway Obstruction',
+    poisoning: 'Poisoning and Overdose',
+    allergy: 'Allergic Reactions',
+    shock: 'Shock Management',
+
+    blsWelcome: 'Welcome to the Basic Life Support (BLS) learning module',
+    blsContent: 'This module will teach you how to perform CPR and use an AED',
+    blsCPR: 'First: Check for Responsiveness, 
     });
 });
 
